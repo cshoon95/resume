5bbba59084523b3cbc0da06c87858eca04e53492
cshoon95 <cshoon921@naver.com>
2023-10-29 19:47:54 +0900

    test

--- src/App.js
+++ src/App.js
@@ -2,24 +2,31 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const outer = document.getElementById('outer');
+  const middle = document.getElementById('middle');
+  const inner = document.getElementById('inner');
+
+  function logEventPhase(event) {
+      console.log(`Event: ${event.type}, Phase: ${event.eventPhase}`);
+  }
+
+  outer.addEventListener('click', logEventPhase, true); // 이벤트 캡처링
+  middle.addEventListener('click', logEventPhase, true); // 이벤트 캡처링
+  inner.addEventListener('click', logEventPhase, true); // 이벤트 캡처링
+
+  outer.addEventListener('click', logEventPhase, false); // 이벤트 버블링
+  middle.addEventListener('click', logEventPhase, false); // 이벤트 버블링
+  inner.addEventListener('click', logEventPhase, false); // 이벤트 버블링
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn Reactasdasdas
-        </a>
-      </header>
+    <div id="outer">
+      Outer
+        <div id="middle">
+            Middle
+            <button id="inner">Inner</button>
+        </div>
     </div>
-  );
-}
+    );
+  }
 
 export default App;
